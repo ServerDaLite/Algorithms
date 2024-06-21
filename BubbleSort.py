d1aa5d204ccd920fe935db08a293c0f447f09afc
ServerDaLite <107884123+ServerDaLite@users.noreply.github.com>
2024-06-21 15:26:53 -0400

    Uploaded BubbleSort.py
    This file contains the Bubble Sort Algorithm.

---
+++ BubbleSort.py
@@ -0,0 +1,24 @@
+from random import randint
+
+def getRandomList(size: int, minimum: int = 1, maximum: int = 100):
+	return [randint(minimum, maximum) for i in range(size)]
+
+def iteration(numbered_list: list, index: int):
+	for idx in range(index-1):
+		if numbered_list[idx] > numbered_list[idx + 1]:
+			numbered_list[idx], numbered_list[idx + 1] = numbered_list[idx + 1], numbered_list[idx]
+	return numbered_list
+
+def bubbleSort(numbered_list: list):
+	for idx in range(len(numbered_list), 1, -1):
+		numbered_list = iteration(numbered_list, idx)
+
+	return numbered_list
+
+
+if __name__ == "__main__":
+	numbered_list: list = getRandomList(10)
+	print(f"Unsorted List: {numbered_list}")
+
+	sorted_list: list = bubbleSort(numbered_list)
+	print(f"Sorted List: {sorted_list}")
